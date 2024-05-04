2d938be99ab3dad55384f33ab5c6ca65232b5f6f
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-05-04 18:22:39 -0300

    1

---
+++ Exercicio_Pessoa_Ano_Bissexto/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Pessoa_Ano_Bissexto
+{
+    internal class Pessoa
+    {
+        string nome;
+        string profissao;
+        DateTime nascimento;
+        public Pessoa()
+        {
+            Console.Write("\nInsira o nome: ");
+            this.nome = Console.ReadLine();
+            Console.Write("\nInsira a profissão: ");
+            this.profissao = Console.ReadLine();
+            Console.Write("\nInsira a data de nascimento: ");
+            this.nascimento = DateTime.Parse(Console.ReadLine());
+        }
+        public DateTime Get_Nascimento()
+        {
+            return nascimento;
+        }
+        public string Get_Nome()
+        {
+            return nome;
+        }
+    }
+}
---
+++ Exercicio_Pessoa_Ano_Bissexto/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio_Pessoa_Ano_Bissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Pessoa pessoa = new Pessoa();
+            TimeSpan dif = now - pessoa.Get_Nascimento();
+            float anos_bissextos = (float)((dif.Days/365.25)/4);
+            Console.WriteLine($"\nA pessoa {pessoa.Get_Nome()} tem {anos_bissextos:0.0} anos bissextos de vida");
+        }
+    }
+}
